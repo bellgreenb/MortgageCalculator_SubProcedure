7f271b86af88969309e435a7535849daad49e977
Sarah Greenberg <68124710+bellgreenb@users.noreply.github.com>
2025-06-07 14:51:31 -0400

    Initial commit

---
+++ frmMortgageCalculator.Designer.vb
@@ -0,0 +1,172 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMortgageCalculator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtLoanAmount = New System.Windows.Forms.MaskedTextBox()
+        Me.txtMonthlyPayment = New System.Windows.Forms.MaskedTextBox()
+        Me.txtYearlyInterestRate = New System.Windows.Forms.MaskedTextBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.cboLoanTerm = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(96, 51)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(109, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Loan Amount:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(112, 97)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(93, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Loan Term: "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(67, 189)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(138, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Monthly Payment: "
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(22, 141)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(183, 20)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Yearly Interest Rate (%):"
+        '
+        'txtLoanAmount
+        '
+        Me.txtLoanAmount.Location = New System.Drawing.Point(227, 45)
+        Me.txtLoanAmount.Name = "txtLoanAmount"
+        Me.txtLoanAmount.Size = New System.Drawing.Size(247, 26)
+        Me.txtLoanAmount.TabIndex = 0
+        '
+        'txtMonthlyPayment
+        '
+        Me.txtMonthlyPayment.Location = New System.Drawing.Point(227, 186)
+        Me.txtMonthlyPayment.Name = "txtMonthlyPayment"
+        Me.txtMonthlyPayment.ReadOnly = True
+        Me.txtMonthlyPayment.Size = New System.Drawing.Size(247, 26)
+        Me.txtMonthlyPayment.TabIndex = 5
+        Me.txtMonthlyPayment.TabStop = False
+        '
+        'txtYearlyInterestRate
+        '
+        Me.txtYearlyInterestRate.Location = New System.Drawing.Point(227, 138)
+        Me.txtYearlyInterestRate.Name = "txtYearlyInterestRate"
+        Me.txtYearlyInterestRate.Size = New System.Drawing.Size(247, 26)
+        Me.txtYearlyInterestRate.TabIndex = 2
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(515, 43)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(92, 36)
+        Me.btnCalculate.TabIndex = 3
+        Me.btnCalculate.Text = "&Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(515, 97)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(92, 36)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(515, 149)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(92, 36)
+        Me.btnClear.TabIndex = 5
+        Me.btnClear.Text = "C&lear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'cboLoanTerm
+        '
+        Me.cboLoanTerm.FormattingEnabled = True
+        Me.cboLoanTerm.Items.AddRange(New Object() {"   30-yr fixed", "   20-yr fixed", "   15-yr fixed"})
+        Me.cboLoanTerm.Location = New System.Drawing.Point(227, 94)
+        Me.cboLoanTerm.Name = "cboLoanTerm"
+        Me.cboLoanTerm.Size = New System.Drawing.Size(247, 28)
+        Me.cboLoanTerm.TabIndex = 1
+        Me.cboLoanTerm.Text = "Please Select a Loan Term"
+        '
+        'frmMortgageCalculator
+        '
+        Me.AcceptButton = Me.btnCalculate
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnExit
+        Me.ClientSize = New System.Drawing.Size(657, 256)
+        Me.Controls.Add(Me.cboLoanTerm)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.txtYearlyInterestRate)
+        Me.Controls.Add(Me.txtMonthlyPayment)
+        Me.Controls.Add(Me.txtLoanAmount)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmMortgageCalculator"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Mortgage Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtLoanAmount As MaskedTextBox
+    Friend WithEvents txtMonthlyPayment As MaskedTextBox
+    Friend WithEvents txtYearlyInterestRate As MaskedTextBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents cboLoanTerm As ComboBox
+End Class
---
+++ frmMortgageCalculator.vb
@@ -0,0 +1,103 @@
+﻿Public Class frmMortgageCalculator
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        If cboLoanTerm.SelectedIndex = -1 Then
+            MessageBox.Show("Please select a valid loan term")
+        End If
+
+        Try
+            Dim loanAmount As Decimal = CDec(txtLoanAmount.Text)
+            Dim loanTerm As Decimal = 0 'loanTerm initialized as 0 years. 
+            Dim yearlyInterestRate As Decimal = CDec(txtYearlyInterestRate.Text)
+            Dim monthlyInterestRate As Decimal = yearlyInterestRate / 12 / 100 'convert yearly rate to monthly to apply to monthly payment.
+            Dim monthlyPayment As Decimal
+            Dim balance As Decimal = loanAmount
+
+            'this if statement evaluates if there is a loan term.
+            Select Case cboLoanTerm.SelectedItem.ToString()
+                Case "   30-yr fixed"
+                    loanTerm = 30
+                Case "   20-yr fixed"
+                    loanTerm = 20
+                Case "   15-yr fixed"
+                    loanTerm = 15
+            End Select
+
+            'monthly payment calculation algorithm
+            Dim totalMonths As Decimal = loanTerm * 12 'convert loanTerm to months for calculation
+
+            'initializing compound factor
+            Dim compoundFactor As Decimal = 1
+
+            'calculate compound factor 
+            For i As Integer = 1 To CInt(totalMonths)
+                compoundFactor *= (1 + monthlyInterestRate)
+            Next
+
+            Me.ComputeMonthlyPayment(monthlyInterestRate, loanAmount, compoundFactor, totalMonths, monthlyPayment)
+
+            If monthlyPayment >= 5000 Then
+                MessageBox.Show("According to common rules, your monthly mortgage payment" & vbNewLine &
+                                "should be no more than 28% of your monthly gross income.")
+            End If
+
+            'display monthly payment in txtMonthlyPayment.Text textbox.
+            txtMonthlyPayment.Text = FormatCurrency(monthlyPayment)
+        Catch ex As InvalidCastException
+            MessageBox.Show("Please check entries for valid numeric data.", ex.GetType.ToString)
+        Catch ex As OverflowException
+            MessageBox.Show("According to common rules, your monthly mortgage payment" & vbNewLine &
+                "should be no more than 28% of your monthly gross income." & vbNewLine & ex.StackTrace, ex.GetType.ToString)
+        Finally
+            txtLoanAmount.Select()
+        End Try
+    End Sub
+
+    Private Sub frmMortgageCalculator_DoubleClick(sender As Object, e As EventArgs) Handles Me.DoubleClick
+        ClearAllTextBoxes()
+    End Sub
+
+    Private Sub ClearAllTextBoxes() Handles txtMonthlyPayment.MouseHover
+        txtLoanAmount.Text = ""
+        Dim loanAmount = 0
+
+        cboLoanTerm.SelectedIndex = -1 ' resets cboLoanTerm selection
+        Dim loanTerm = 0
+        cboLoanTerm.Text = "Please select a loan Term"
+
+        txtYearlyInterestRate.Text = ""
+        Dim yearlyInterestRate = 0
+
+        txtMonthlyPayment.Text = ""
+        Dim monthlyPayment = 0
+    End Sub
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtLoanAmount.Text = ""
+        Dim loanAmount = 0
+
+        cboLoanTerm.SelectedIndex = -1 ' resets cboLoanTerm selection
+        Dim loanTerm = 0
+        cboLoanTerm.Text = "Please select a loan Term"
+
+        txtYearlyInterestRate.Text = ""
+        Dim yearlyInterestRate = 0
+
+        txtMonthlyPayment.Text = ""
+        Dim monthlyPayment = 0
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub ComputeMonthlyPayment(monthlyInterestRate As Decimal, loanAmount As Decimal, compoundFactor As Decimal, totalMonths As Decimal, ByRef monthlyPayment As Decimal)
+        'calculate monthly payment using the initialized & calculated compound factor
+        If monthlyInterestRate > 0 Then
+            monthlyPayment = loanAmount * (monthlyInterestRate * compoundFactor) / (compoundFactor - 1)
+        Else
+            monthlyPayment = loanAmount / totalMonths
+        End If
+    End Sub
+
+
+End Class
